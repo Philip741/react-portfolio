72395ecad3388fae3570503b5ef9030f125a7a94
Philip Browning <philip741@gmail.com>
2022-01-07 18:14:38 -0500

    Test to determine if chakra UI is functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+import { Box } from '@chakra-ui/react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <Box bg='tomato' w='100%' p={4} color='white'>
+            This is the Box
+        </Box>
     </div>
   );
 }
